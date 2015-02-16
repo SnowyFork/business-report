b7df08478b89184648788e4a53581ad5fb43363c
SnowyFork <cabuhandah@mail.saintpeters.edu>
2015-02-16 16:18:35 -0500

    Create Hospital.cpp

---
+++ HospitalCpp/SourceFiles/Hospital.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+#include "Hospital.h"
+
+using namespace std;
+
+double Hospital::rent=355;
+int Hospital::count=0;
+
+Hospital::Hospital(string ID2, string nam, double dayz)
+{
+	ID = ID2;
+	name = nam;
+	days = dayz;
+}
+
+void Hospital::compute()
+{
+	amountdue = rent * days;
+	count++;
+}
+
+int Hospital::getcount()
+{
+	return count;
+}
+
+void Hospital::printdata()
+{
+	cout << ID << setw(10) << name << setw(10) << days << setw(10) << amountdue << endl;
+}
