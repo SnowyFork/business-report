fe68afa9b9f637329a68caf0f819522e40562d2e
SnowyFork <cabuhandah@mail.saintpeters.edu>
2015-02-16 16:16:08 -0500

    Create Hospital.h

---
+++ HospitalCpp/Header/Hospital.h
@@ -0,0 +1,23 @@
+#ifndef HOSPITAL_H
+#define HOSPITAL_H
+
+#include <string>
+
+using namespace std;
+
+class Hospital
+{
+	public:
+		Hospital(string, string, double);
+
+		void printdata();
+		static int getcount();
+		void compute();
+		
+	private:
+		string ID, name;
+		static int count;
+		double days, amountdue;
+		static double rent;
+};
+#endif
